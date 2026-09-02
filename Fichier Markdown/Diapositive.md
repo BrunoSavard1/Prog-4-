95bd85cf216caf3cc3b583cb29b75a60bf09fa42
Bruno Savard <brunosavard112@gmail.com>
2026-09-02 14:48:59 -0400

    Add files via upload

---
+++ Fichier Markdown/Diapositive.md
@@ -0,0 +1,13 @@
+
+# Diapositive 1 
+Pourquoi Elden Ring est le meilleur jeu 
+
+Slide::
+
+# Diapositive 2
+Pourquoi ER défonce GOW 
+
+Slide:: 
+# Diapositives 3
+blablabla
+
---
+++ Fichier Python/fonction diapo.py
@@ -0,0 +1,43 @@
+import mistletoe
+import mistletoe
+
+
+def convertir_diapositive(fichier_md, fichier_html):
+    """
+    Convertit un fichier Markdown contenant des 'Slide::'
+    en un fichier HTML contenant une div par diapositive.
+    """
+
+    # Lire le fichier Markdown
+    with open(fichier_md, 'r', encoding='utf-8') as contenu:
+        texte = contenu.read()
+
+    # Séparer les diapositives à chaque 'Slide::'
+    slides = texte.split("Slide::")
+
+    # CSS des diapositives
+    css = """
+    <style>
+        .slide {
+            background-color: rgb(200, 184, 189);
+            width: 100vw;
+            height: 70vh;
+        }
+    </style>
+    """
+
+    resultat = ""
+
+    # Convertir chaque slide en HTML
+    for slide in slides:
+        rendu = mistletoe.markdown(slide)
+
+        slide_html = '<div class="slide">' + rendu + '</div>'
+
+        resultat += slide_html
+
+    # Écrire le résultat dans le fichier HTML
+    with open(fichier_html, 'w', encoding='utf-8') as fout:
+        fout.write(css + resultat)
+
+convertir_diapositive("Diapositive.md", "Slide.html")
